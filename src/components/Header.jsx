1e82c83aa8971a78655dd727fdb99a9eee8476c8
lavanyasoundar17 <lavanyaprdp17@gmail.com>
2024-11-04 15:09:19 +0000

    added sorting

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import '../styles/Header.css'
+
+
+const Header =()=>{
+    return(
+        <div>
+        <div className="header-text">
+            <h1>Northcoders News</h1>
+            </div>
+            <nav className="nav-links">
+                <Link to = "/home">Home</Link>
+                <Link to = "/articles">Articles</Link>
+                <Link to = "/users">Users</Link>
+                <Link to = "/topics">Topics</Link>
+
+            </nav>
+            </div>
+
+    )
+}
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,66 @@
+import {useState,useEffect} from 'react'
+import { Link } from "react-router-dom"
+import '../styles/Home.css'
+
+
+
+const Home = () => {
+    const [articles, setArticles] = useState([]);
+    const [sortOption, setSortOption] = useState("date"); 
+
+    const fetchArticles = async () => {
+    
+        try {
+            const response = await fetch('http://localhost:9090/api/articles');
+            const data = await response.json();
+            setArticles(data.articles);
+        } catch {
+            console.error("Error fetching articles");
+        }
+    };
+
+useEffect(() => {
+    fetchArticles();
+}, []);
+
+useEffect(() => {
+    if (articles.length > 0) {
+        let sortedArticles = [...articles];
+        if (sortOption === "date") {
+            sortedArticles.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+        } else if (sortOption === "title") {
+            sortedArticles.sort((a, b) => a.title.localeCompare(b.title));
+        } else if (sortOption === "author") {
+            sortedArticles.sort((a, b) => a.author.localeCompare(b.author));
+        }
+        setArticles(sortedArticles);
+    }
+}, [sortOption, articles.length]);
+
+return (
+    <div className="welcome_text">
+        <h2>Welcome to NC news!</h2>
+        <div>
+            <label>Sort by: </label>
+            <select onChange={(e) => setSortOption(e.target.value)}>
+                <option value="date">Date</option>
+                <option value="title">Title</option>
+                <option value="author">Author</option>
+            </select>
+        </div>
+        <ul className="articles_list">
+            {articles.map((article, index) => (
+                <div key={index} className="article_container">
+                    <h3>
+                        <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+                    </h3>
+                    <img src={article.article_img_url} alt={article.title} />
+                    <p>By {article.author}</p>
+                </div>
+            ))}
+        </ul>
+    </div>
+);
+};
+
+export default Home;
